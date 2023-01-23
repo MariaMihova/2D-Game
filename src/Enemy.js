52b9d821ea94f75d8f4d26e7ed33b39b66e80492
MariaMihova <m.ivanova1012@gmail.com>
2023-01-23 11:36:07 +0200

    Enemy 1 animation

--- src/Enemy.js
+++ src/Enemy.js
@@ -6,20 +6,37 @@ class Enemy {
     this.markedForDelition = false;
     this.lives = 10;
     this.score = this.lives;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame = 37;
   }
 
   update() {
-    this.x += this.speedX;
+    this.x += this.speedX - this.game.speed;
     if (this.x + this.width < 0) {
       // no width property
       this.markedForDelition = true;
     }
+    this.frameX < this.maxFrame ? this.frameX++ : (this.frameX = 0);
   }
 
   draw(context) {
-    context.fillStyle = "red";
-    context.fillRect(this.x, this.y, this.width, this.height); //  y, widht, height declated in the child classes
-    context.fillStyle = "black";
+    // context.fillStyle = "red";
+    if (this.game.debug) {
+      context.strokeRect(this.x, this.y, this.width, this.height); //  y, widht, height declated in the child classes
+    }
+    // context.fillStyle = "black";
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
     context.font = "20px Helvetica";
     context.fillText(this.lives, this.x, this.y);
   }
@@ -28,8 +45,10 @@ class Enemy {
 export default class Angler1 extends Enemy {
   constructor(game) {
     super(game);
-    this.width = 228 * 0.2;
-    this.height = 169 * 0.2;
+    this.width = 228;
+    this.height = 169;
     this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    this.image = document.getElementById("enemy1");
+    this.frameY = Math.floor(Math.random() * 3);
   }
 }
